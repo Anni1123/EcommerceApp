3e5fd869c8c986cc76584d1af407167fa97ce8d8
Anni1123 <kumarianni1213@gmail.com>
2020-03-24 16:31:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/coursebuy/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.coursebuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login_button,joinnow_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login_button=(Button)findViewById(R.id.main_login_btn);
+        joinnow_button=(Button)findViewById(R.id.main_join_now_btn);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
+    }
+}
