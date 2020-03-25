98b619066026c83634c3373f4e3afa0898dfd971
Anni1123 <kumarianni1213@gmail.com>
2020-03-25 19:07:29 +0530

    SettingActivity

--- app/src/main/java/com/example/coursebuy/Model/Users.java
+++ app/src/main/java/com/example/coursebuy/Model/Users.java
@@ -5,15 +5,6 @@ public class Users {
     }
 
     private String phone;
-    private String name;
-
-    public Users(String phone, String name, String password) {
-        this.phone = phone;
-        this.name = name;
-        this.password = password;
-    }
-
-    private String password;
 
     public String getPhone() {
         return phone;
@@ -38,4 +29,35 @@ public class Users {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String name;
+    private String password;
+    private String image;
+
+    public Users(String phone, String name, String password, String image, String address) {
+        this.phone = phone;
+        this.name = name;
+        this.password = password;
+        this.image = image;
+        this.address = address;
+    }
+
+    private String address;
+
 }
